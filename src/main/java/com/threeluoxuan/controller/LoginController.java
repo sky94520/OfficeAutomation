05cd0215a2b891d4ffababdf1463c36af1dd9736
coke_1996 <18796360983zs@gmail.com>
2019-08-21 22:29:29 +0800

    by zhang : 添加登录拦截的功能

--- src/main/java/com/threeluoxuan/controller/LoginController.java
+++ src/main/java/com/threeluoxuan/controller/LoginController.java
@@ -2,13 +2,11 @@ package com.threeluoxuan.controller;
 
 import org.activiti.engine.IdentityService;
 import org.activiti.engine.identity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -16,11 +14,7 @@ public class LoginController {
 
     @Resource
     private IdentityService identityService;
-
-//    @Autowired
-//    private HttpServletRequest request;
-
-//    private HttpSession session = request.getSession();
+    
 
     /*
     跳转至登录页面
@@ -37,7 +31,8 @@ public class LoginController {
     @RequestMapping("/logout")
     public ModelAndView logout(HttpSession httpSession) {
         httpSession.removeAttribute("user");
-        return this.login();
+        ModelAndView view = new ModelAndView("login");
+        return view;
     }
 
     @PostMapping(value = "/validate")
