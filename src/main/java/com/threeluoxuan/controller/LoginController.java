ec7dc521366810898d0581a9c46b96b0ea5189b1
sky94520 <541052067@qq.com>
2019-08-21 21:25:01 +0800

    by xiaoniu 删除了UserService.java

--- src/main/java/com/threeluoxuan/controller/LoginController.java
+++ src/main/java/com/threeluoxuan/controller/LoginController.java
@@ -1,29 +1,19 @@
 package com.threeluoxuan.controller;
 
-import com.threeluoxuan.Service.UserService;
 import org.activiti.engine.IdentityService;
 import org.activiti.engine.identity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
 
-    @Autowired
-    private UserService userService;
     @Resource
     private IdentityService identityService;
-
-//    @Autowired
-//    private HttpServletRequest request;
-
-//    private HttpSession session = request.getSession();
 
     /*
     跳转至登录页面
